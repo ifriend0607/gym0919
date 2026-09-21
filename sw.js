674366e3a0523b6a8ad343182bbb12a8df34fe9d
ifriend0607 <ifriend0607@gmail.com>
2026-09-21 01:57:07 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+// 같은 주소(ifriend0607.github.io)의 다른 앱(gym0607) 캐시는 건드리지 않도록 'wf-'로 시작하는 것만 정리
+const CACHE = 'wf-v2';
+const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k.startsWith('wf-') && k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(fetch(e.request).then(r => { const c = r.clone(); caches.open(CACHE).then(cache => cache.put(e.request, c)); return r; }).catch(() => caches.match(e.request)));
+});
